5eeb55a8e738b127a680e34f118399de8d6162b7
MoustafaAmer12 <moustafa3sam20113@gmail,com>
2023-07-13 17:22:54 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a given string
+ *
+ * Return: 0 or success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
