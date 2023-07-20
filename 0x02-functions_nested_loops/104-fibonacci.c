c4fa77ff92efb5b1e5f89e8e41d9531c3045da10
MoustafaAmer12 <moustafa3sam20113@gmail,com>
2023-07-19 19:01:53 -0700

    Task 16 final isa

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,19 +8,19 @@
 int main(void)
 {
 	int i;
-	long double fib1, fib2, term;
+	unsigned long fib1, fib2, term;
 
 	i = 2;
 	fib1 = 1;
 	fib2 = 2;
-	printf("%.0Lf, %.0Lf, ", fib1, fib2);
-	while (i < 100)
+	printf("%lu, %lu, ", fib1, fib2);
+	while (i < 98)
 	{
 		term = fib1 + fib2;
 		fib1 = fib2;
 		fib2 = term;
-		printf("%.0Lf", term);
-		if (i != 99)
+		printf("%lu", term);
+		if (i != 97)
 			printf(", ");
 		i++;
 	}
