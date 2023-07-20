2ee61ae7738ef66334839e14de82b9ebf3478de7
MoustafaAmer12 <moustafa3sam20113@gmail,com>
2023-07-19 18:21:16 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - calculates the sum of the even fibonacci terms less than 4000000
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int fib1, fib2, term, max;
+	long sum;
+
+	max = 4000000;
+	fib1 = 1;
+	fib2 = 2;
+	sum = fib2;
+	term = fib1 + fib2;
+	while (term <= 4000000)
+	{
+		if (term % 2 == 0)
+			sum += term;
+		fib1 = fib2;
+		fib2 = term;
+		term = fib1 + fib2;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
