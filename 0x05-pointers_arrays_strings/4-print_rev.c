b4bcb1f85975b3c8bbc8ea17ec366676f1f9fb3d
MoustafaAmer12 <moustafa3sam20113@gmail,com>
2023-07-25 20:21:45 -0700

    Task 4 edit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,15 @@ void print_rev(char *s)
 {
 	int len;
 
-	len = _strlen(s) - 1;
+	len = 0;
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
 	while (len >= 0)
 	{
 		_putchar(*(s + len));
+		len--;
 	}
 	_putchar('\n');
 }
