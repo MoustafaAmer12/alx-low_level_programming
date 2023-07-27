de48c21d246cadd6b15c63f06b5f4bd0fe59d111
MoustafaAmer12 <moustafa3sam20113@gmail,com>
2023-07-26 19:58:40 -0700

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - converts the digits in the string to int
+ * @s: string to check for decimals
+ *
+ * Return: int value found and 0 if none.
+ */
+
+int _atoi(char *s)
+{
+	int pos = 0;
+	int num = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] <= 57 && s[i] >= 48)
+		{
+			while (s[i] <= 57 && s[i] >= 48 && s[i] != '\0')
+			{
+				num = num * 10 + (s[i] - '0');
+				i++;
+			}
+			if (pos % 2 == 1)
+				num = -num;
+			return (num);
+		}
+		else if (s[i] == '-')
+		{
+			pos++;
+		}
+		i++;
+	}
+	return (0);
+}
