22e196c2da13c3fd02b1fccbe2b8bb118cbe835f
MoustafaAmer12 <moustafa3sam20113@gmail,com>
2023-07-27 20:42:50 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes the string by rotating any character with 13
+ * @str: string literal to be encoded
+ *
+ * Return: pointer to the first character of the encoded string
+ */
+
+char *rot13(char *str)
+{
+	char *test = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *conv = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (j < 52)
+		{
+			if (test[j] == str[i])
+			{
+				str[i] = conv[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
