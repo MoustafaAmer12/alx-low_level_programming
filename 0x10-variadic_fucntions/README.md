63ee7deeb89d44daa95b7ac2a4b427a77453bc6c
MoustafaAmer12 <126791188+MoustafaAmer12@users.noreply.github.com>
2023-08-19 04:51:45 +0300

    Delete 0x10-variadic_fucntions directory

--- 0x10-variadic_fucntions/README.md
+++
@@ -1,3 +0,0 @@
-# 0X10 Variadic Function
-## Description
-This directory is destinated to the task 0x10-variadic_functions.
--- 0x10-variadic_fucntions/variadic_functions.h
+++
@@ -1,7 +0,0 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
-
-
-int sum_them_all(const unsigned int n, ...);
-
-#endif
