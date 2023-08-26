4ea8474b93d4f36f36c11283482738aead122c64
MoustafaAmer12 <moustafa3sam20113@gmail.com>
2023-08-25 19:03:12 -0700

    Task 0 edit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,21 @@
 #include "lists.h"
 #include <stdio.h>
+/**
+ * _strlen - returns the size of a string
+ * @str: the string to return its size
+ *
+ * Return: size
+ */
+int _strlen(char *str)
+{
+	size_t size = 0;
+
+	if (!str)
+		return (0);
+	while (*str++)
+		size++;
+	return (size);
+}
 
 /**
  * print_list - prints the list from the beginning to the end
@@ -13,7 +29,7 @@ size_t print_list(const list_t *h)
 	
 	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str ? h->str : "(nil)");
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
 		size++;
 	}
