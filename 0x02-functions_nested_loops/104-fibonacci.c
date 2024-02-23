bd8d2246c44937e40af52be68cbf3a5474d78ca4
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-02-23 10:30:23 -0800

    Task 16 Final

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * num_length - returns the length of a number
+ * @num: number to compute its length
+ *
+ * Return: length of n
+ */
+int num_length(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length++;
+	}
+	return (length);
+}
 
 /**
  * main - Entry point
@@ -7,20 +28,30 @@
  */
 int main(void)
 {
-	unsigned long first = 1, second = 2, num;
-	int i = 2;
+	unsigned long first = 1, second = 2, num, mx = 100000000, fo = 0, so = 0, sumo = 0;
+	int i, initials;
 
-	printf("%lu, %lu, ", first, second);
-	while (i < 98)
+	for (i = 1; i <= 98; i++)
 	{
-		num = first + second;
-		if (i != 97)
-			printf("%lu, ", num);
-		else
-			printf("%lu\n", num);
+		if (fo > 0)
+			printf("%lu", fo);
+		initials = num_length(mx) - 1 - num_length(first);
+		while (fo > 0 && initials > 0)
+		{
+			printf("%d", 0);
+			initials--;
+		}
+		printf("%lu", first);
+		num = (first + second) % mx;
+		sumo = fo + so + (first + second) / mx;
 		first = second;
+		fo = so;
 		second = num;
-		i++;
+		so = sumo;
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
