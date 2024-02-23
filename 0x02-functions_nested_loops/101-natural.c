c36f29686c52ff7e87be6d3301a2b847abebb573
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-02-23 09:02:33 -0800

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int sum = 0;
+	int i = 1;
+
+	while (i * 3 < 1024 || i * 5 < 1024)
+	{
+		if (i * 3 < 1024)
+			sum += (i * 3);
+		if (i * 5 < 1024)
+			sum += (i * 5);
+		i++;
+	}
+	return (0);
+}
