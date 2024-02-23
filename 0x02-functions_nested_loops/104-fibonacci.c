ef0034a6d33133db6106749498611172554c8e05
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-02-23 09:47:01 -0800

    Task 16 edit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,14 +10,14 @@ int main(void)
 	double first = 1, second = 2, num;
 	int i = 2;
 
-	printf("%.0lf, %.0lf, ", first, second);
+	printf("%.0f, %.0f, ", first, second);
 	while (i < 98)
 	{
 		num = first + second;
 		if (i != 97)
-			printf("%.0lf, ", num);
+			printf("%.0f, ", num);
 		else
-			printf("%.0lf\n", num);
+			printf("%.0f\n", num);
 		first = second;
 		second = num;
 		i++;
