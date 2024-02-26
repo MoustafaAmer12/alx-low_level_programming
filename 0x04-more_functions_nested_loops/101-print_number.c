ca4b63c15132cbdc67e20ad8f45760feb01b0e93
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-02-26 17:25:00 +0200

    Task 12 edit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,41 +8,18 @@
  */
 void print_number(int n)
 {
-	int end = 0,  sign = 1;
-	unsigned int num, rev = 0;
+	unsigned int num;
 
-	if (n == 0)
-	{
-		_putchar(48);
-		return;
-	}
 	if (n < 0)
 	{
-		sign = -1;
-		n *= -1;
-	}
-	num = n;
-	while (num % 10 == 0)
-	{
-		end++;
-		num /= 10;
-	}
-	while (num > 0)
-	{
-		rev *= 10;
-		rev += num % 10;
-		num /= 10;
-	}
-	if (sign == -1)
+		num = -1*n;
 		_putchar('-');
-	while (rev > 0)
-	{
-		_putchar(rev % 10 + 48);
-		rev /= 10;
 	}
-	while (end > 0)
+	else
 	{
-		_putchar(48);
-		end--;
+		num = n;
 	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar(num % 10 + 48);
 }
