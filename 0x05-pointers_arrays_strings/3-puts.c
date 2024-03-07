13ad85c7256458c554e63ba6634df7024cd343c2
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-03-07 16:02:14 +0200

    Tasks 0-3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: pointer to the first character of the string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*(str + i) != '\0')
+	{
+		_putchar(str + i);
+	}
+	_putchar('\n');
+}
