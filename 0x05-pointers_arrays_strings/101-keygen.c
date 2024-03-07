c77b48f8aaae0d9813d53062ffba54794d4e6c45
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-03-07 17:59:21 +0200

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - entry point
@@ -7,7 +9,16 @@
  */
 int main(void)
 {
+	int sum;
+	char c;
 
-
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
 	return (0);
 }
