d6cc2c41167a10f33fb18866e65ffa27889ecde9
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-03-21 19:39:54 +0200

    Task 0 betty edit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t size = 0;
+
 	if (!h)
-		return size;
+		return (size);
 	do {
 		printf("%d\n", h->n);
 		size++;
 		h = h->next;
-	}while (h);
+	} while (h);
 	return (size);
 }
