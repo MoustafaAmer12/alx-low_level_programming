2b8d5ce626e849229b074221f252b352110ee3e6
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-03-21 19:37:58 +0200

    Task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints a doubly linked list of integers
+ * @h: pointer to the head of the list
+ *
+ * Return: size of the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size = 0;
+	if (!h)
+		return size;
+	do {
+		printf("%d\n", h->n);
+		size++;
+		h = h->next;
+	}while (h);
+	return (size);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# Doubly Linked Lists
+## 0x17 - C, Doubly Linked Lists
+This directory is designated for doubly linked lists implementation tasks in C.
