1e6eff1731f83d0bbab7d316aa206f9863d0a78a
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-05-05 04:07:42 +0300

    Task 2 edits

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,10 +16,13 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 		if (src[i] == '\0')
+		{
+			i++;
 			break;
+		}
 		i++;
 	}
-	while (i++ < n)
-		dest[i] = '\0';
+	while (i < n)
+		dest[i++] = '\0';
 	return (dest);
 }
