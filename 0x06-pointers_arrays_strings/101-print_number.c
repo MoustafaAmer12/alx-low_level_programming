e78fe09b37be41a90d88e584979a5881ceea2ff1
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-05-05 05:34:02 +0300

    Task 9 edit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_int - Prints a number recursively
+ * put_int - Prints a number recursively
  * @n: Integer to be printed
  * @sign: 1 for negative and 0 for positive
  *
  * Return: void
  */
-void print_int(unsigned int n, unsigned int sign)
+void put_int(unsigned int n, unsigned int sign)
 {
 	char digit = n % 10 + '0';
 
 	if (n >= 10)
-		print_int(n / 10, sign);
+		put_int(n / 10, sign);
 	if (sign && n < 10)
 		_putchar('-');
 	_putchar(digit);
@@ -26,7 +26,7 @@ void print_int(unsigned int n, unsigned int sign)
 void print_number(int n)
 {
 	if (n < 0)
-		print_int(-1 * n, 1);
+		put_int(-1 * n, 1);
 	else
-		print_int(n, 0);
+		put_int(n, 0);
 }
