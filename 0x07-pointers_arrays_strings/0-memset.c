5ec6c718cbb591208777daa4ed9cbc590fcbe37c
MoustafaAmer12 <moustafaesam20113@gmail.com>
2024-05-09 02:26:44 +0300

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - Fills a number of bytes in memory with a constant byte
+ * @s: pointer to the location to be filled
+ * @b: byte to fill the memory
+ * @n: number of bytes to be filled
+ *
+ * Return: pointer to the filled memory location
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while(n--)
+	{
+		*s = b;
+		s++;
+	}
+	return (ptr);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+# Pointers, Arrays & Strings
+## 0x07 - C, Even more pointers, arrays & strings
+This directory is designated for extra pointers task for th ALX-SE Program.<br>
+This project includes strings tasks, besides copying data using ponters and so on.
